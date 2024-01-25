45b97627b31d5af0fef607351ee814487bff07be
Tanjil Ahamed <tanjil113355@gmail.com>
2024-01-25 12:42:59 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './Layout/Home/Home.jsx';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
